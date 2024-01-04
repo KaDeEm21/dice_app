6f6598b445d56d6fd18f959e6aa5bff871205314
KaDeEm21 <kadeem393@gmail.com>
2024-01-04 21:41:58 +0100

    Added background and dice image from local assets directory

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade300,
+        body: Center(
+          child: Image.asset('assets/images/dice-1.png', width: 300,),
+        ),
+      ),
+    );
+  }
+}
+
