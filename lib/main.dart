272bf70407d0c7646fecf4e090ef4fdc6aa95f35
KaDeEm21 <kadeem393@gmail.com>
2024-03-04 15:28:27 +0100

    - 'Unable to access asset' problem fixed. - Changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue.shade300,
+        backgroundColor: Colors.grey.shade800,
         body: Center(
-          child: Image.asset('assets/images/dice-1.png', width: 300,),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Image.asset('assets/images/dice-2.png', width: 200,),
+              const TextButton(
+                onPressed: null, child: Text(
+                "ROLL DICE", style: TextStyle(fontStyle: FontStyle.normal, fontSize: 30, color: Colors.white),
+              ),
+              )
+            ],
+          ),
         ),
       ),
     );
